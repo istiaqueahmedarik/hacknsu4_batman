53923dd9f18bd4f1732364430a9212b9731a2326
istiaque_ahmed <istiaqueahmedarik@gmail.com>
2022-08-19 14:08:51 +0600

    update 2:00

--- main_app/pages/index.js
+++ main_app/pages/index.js
@@ -1,4 +1,6 @@
+import { Button } from '@mui/material';
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import Image from 'next/image';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import app from '../firebase';
 export default function Home() {
@@ -16,25 +18,33 @@ export default function Home() {
   };
   if (loading) {
     return (
-      <div>
+      <div className="primary">
         <p>Initialising User...</p>
       </div>
     );
   }
   if (error) {
     return (
-      <div>
+      <div className="primary">
         <p>Error: {error}</p>
       </div>
     );
   }
   if (user) {
     return (
-      <div>
+      <div  className="primary">
+        <div>
         <p>Current User: {user.email}</p>
         <button onClick={logout}>Log out</button>
+        </div>
+        
       </div>
     );
   }
-  return <button onClick={login}>Log in</button>;
+  return(<>
+  <Image src="https://media.istockphoto.com/photos/demo-sign-cubes-picture-id615422436?k=20&m=615422436&s=612x612&w=0&h=m-VHyxKGp3qdXyi_O5C1oxghcmbAegmb34VaD9n6v7c=" alt="logo"/>
+  <Button variant="outlined"  onClick={login}>Log in</Button>
+
+  
+  </>) 
 }
